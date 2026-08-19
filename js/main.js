63730a87eba23fe349cf1159d9b4278513aaf72d
kurogi-souta-hydra <m2401016@msg.ac.jp>
2026-08-19 11:36:49 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,9 +6,28 @@ $(function(){
     $(this).toggleClass("active");
     $("#header .navi").toggleClass("active");
   });
-  
   $(".navi a").click(function () {
     $(".hamburger").removeClass("active");
     $("#header .navi").removeClass("active");
   });
+
+  /*-------------------------------
+  スクロール時のスライド
+  ---------------------------------*/
+  const slideElements = document.querySelectorAll(
+    '.slideInL-img, .slideInR-img'
+  );
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('is-visible');
+      }
+    });
+  }, {
+    threshold: 0
+  });
+  slideElements.forEach((element) => {
+    observer.observe(element);
+  });
+
 });
